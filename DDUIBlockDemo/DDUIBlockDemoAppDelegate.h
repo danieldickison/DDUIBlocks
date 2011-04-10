3d1a7fee158a42c32f8e1e5fdac860b4bee367c2
Daniel Dickison <danieldickison@gmail.com>
2011-04-10 17:24:06 -0400

    Initial Commit

---
+++ DDUIBlockDemo/DDUIBlockDemoAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  DDUIBlockDemoAppDelegate.h
+//  DDUIBlockDemo
+//
+//  Created by Daniel Dickison on 4/10/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DDUIBlockDemoAppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@property (nonatomic, retain) IBOutlet UINavigationController *navigationController;
+
+@end
---
+++ DDUIBlockDemo/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  RootViewController.h
+//  DDUIBlockDemo
+//
+//  Created by Daniel Dickison on 4/10/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RootViewController : UITableViewController {
+
+}
+
+
+@end
